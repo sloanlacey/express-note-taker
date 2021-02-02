22721f00ac426d20fb5e32ab5d5714e0818ee6a1
sloanlacey <sloanlacey89@gmail.com>
2021-02-01 20:07:00 -0800

    Add html routes, page now opens to the index.html as a default unless /notes is specifically addressed.

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-});
- 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
-});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require('./routing/html-routes.js')(app);
 
 app.listen(PORT, function() {
     console.log('App listening on PORT: ' + PORT);
