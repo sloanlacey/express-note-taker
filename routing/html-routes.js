ee453959a3547a6bed20a69dd201b41525845060
sloanlacey <sloanlacey89@gmail.com>
2021-02-02 12:29:44 -0800

    HTML routes updated and working, navigation buttons on app are now functional.

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,14 +1,14 @@
 const path = require('path');
-const fs = require('fs');
+// const fs = require('fs');
 
 module.exports = function (app) {
     
-    app.get('*', function(req,res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+    app.get('/', function(req,res) {
+        res.sendFile(path.join(__dirname, '/../public/index.html'));
     });
 
-    app.get('/notes', function(req,res) {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    app.get('/notes', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../public/notes.html'));
     });
 
 }
